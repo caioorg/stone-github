30deef20e2761c9a7748108cd3ac3d985dbd3580
Caio Almeida <caio.almeida@opah.com.br>
2020-01-28 22:03:13 -0300

    feat adding a validation if the application breaks does not leave a white screen for the user and a return informing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ErrorBoundary } from '@/molecules'
 import Routes from '@/routes'
 
 ReactDOM.render(
   <Router>
-    <Routes />
+    <ErrorBoundary>
+      <Routes />
+    </ErrorBoundary>
   </Router>,
   document.getElementById('root')
 )
---
+++ src/molecules/ErrorBoundary/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: false
+    }
+  }
+
+  componentDidCatch(error) {
+    console.error(error)
+    this.setState({ error: true })
+  }
+
+  render() {
+    const { error } = this.state
+    return error ? <div>Aplicação Indisponivel</div> : this.props.children
+  }
+}
+
+export default ErrorBoundary
