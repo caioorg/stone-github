809fa4eac5990c484095a74b25a9574cd66ded51
Caio Almeida <caio.almeida@opah.com.br>
2020-01-30 22:34:03 -0300

    Updates

---
+++ precache-manifest.db878992ae27e153ff06183ffb050932.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9c6d20dffbb2cb8e7daa848bba6af5",
+    "url": "/stone-github/index.html"
+  },
+  {
+    "revision": "1d483521417a93acf676",
+    "url": "/stone-github/static/js/2.2a41abf8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/stone-github/static/js/2.2a41abf8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebf029afcda6cea316f3",
+    "url": "/stone-github/static/js/main.d2069433.chunk.js"
+  },
+  {
+    "revision": "f91a186307dff1eff59e",
+    "url": "/stone-github/static/js/runtime-main.8bc900ed.js"
+  },
+  {
+    "revision": "154ae504656c84835a3a7ee8e11d2c56",
+    "url": "/stone-github/static/media/404.154ae504.png"
+  },
+  {
+    "revision": "bab8afa41e5f9a8344651f282f4ec381",
+    "url": "/stone-github/static/media/sprite.bab8afa4.svg"
+  }
+]);
